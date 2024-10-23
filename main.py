c74aaca123690b0068be8c6bb8dd44822280225f
vinodkamisetti <vinodkrish.k@gmail.com>
2024-10-23 11:07:11 +0200

    added secrets

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import argparse
 with open("./config/config.yml",'r') as file:
     config = yaml.safe_load(file)
 
-with open("./../config/secrets.yml", "r") as file:
+with open("./config/secrets.yml", "r") as file:
     secrets = yaml.safe_load(file)
 
 os.environ['MLFLOW_TRACKING_URI'] = secrets['mlflow']['MLFLOW_TRACKING_URI']
@@ -24,3 +24,5 @@ def main(run_name):
 if __name__ =="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--run_name", type=str, default=run_name)
+    args = parser.parse_args()
+    main(args.run_name)
--- src/Data_Cleaning.py
+++ src/Data_Cleaning.py
@@ -22,7 +22,7 @@ def data_cleaning(raw_data_path, clean_data_path):
     clean_data_file = os.path.join(clean_data_path,raw_data_file)
     df.to_csv(clean_data_file, index=False)
     mlflow.log_param("cleaned_data_path", clean_data_file)
-    print("Data Cleaning Finished")
+    print("#####Data Cleaning Finished#####")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -30,4 +30,4 @@ if __name__=="__main__":
     parser.add_argument("--clean_data_path", help="clean data path", default=clean_data_path)
     #parser.add_argument("--raw_data_file", help="provide raw data file name",default=raw_data_file)
     args = parser.parse_args()
-    data_cleaning(args.raw_data_path, args.clean_data_path, )
+    data_cleaning(args.raw_data_path, args.clean_data_path)
